69baec1ccde4bea0fc1bbfc557b79ec40a5d56d1
Victor Scherbakov <victor@unigine.com>
2023-04-05 12:54:39 +0700

    Script to install yandex disk

---
+++ script/install_yandex_disk.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -x "$(command -v wget)" ]; then
+    echo "'wget' installation has been started."
+    sudo apt update
+    sudo apt install wget
+else
+    echo "Skip 'wget' installation"
+fi
+
+if [ ! -x "$(command -v yandex-disk)" ]; then
+    echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/yandex-disk.list > /dev/null && wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- | sudo apt-key add - && sudo apt-get update && sudo apt-get install -y yandex-disk
+    yandex-disk setup
+else
+    echo "Skip 'yandex-disk' installation"
+fi
+
