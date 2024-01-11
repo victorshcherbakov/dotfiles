db3c5b3d8399eb824ea105178da2c889cb953e89
Victor <victor@unigine.com>
2024-01-11 17:08:31 +0700

    wp

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,30 +1,48 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-      ensure_installed = {
-        "cpp",
-        "rust",
-        "go",
-        "lua",
-        "python",
-        "cmake",
-        "javascript",
-      },
-      highlight = { -- enable highlighting for all file types
-        enable = true, -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  cmd = {
+    "TSInstall",
+    "TSInstallSync",
+    "TSInstallInfo",
+    "TSUpdate",
+    "TSUpdateSync",
+    "TSUninstall",
+    "TSBufEnable",
+    "TSBufDisable",
+    "TSBufToggle",
+    "TSEnable",
+    "TSDisable",
+    "TSToggle",
+    "TSModuleInfo",
+    "TSEditQuery",
+    "TSEditQueryUserAfter",
   },
-
-  {
+  build = ":TSUpdate",
+  dependencies = {
     "nvim-treesitter/nvim-treesitter-context",
-    opts = {
-      separator = "―",
+    -- "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  opts = {
+    ensure_installed = {
+      "c",
+      "cpp",
+      "rust",
+      "go",
+      "lua",
+      "python",
+      "cmake",
+      "bash",
+      "javascript",
+    },
+    highlight = { -- enable highlighting for all file types
+      enable = true, -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
+    },
+    indent = { -- indentation based on treesitter for the |=| operator.
+      enable = false,
     },
   },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
