7fa97000321ba29916d34df2e731e2cef38c62db
Victor Shcherbakov <victor@unigine.com>
2026-02-12 22:08:58 +0700

    Fixed downloading neovim for Linux Mint

--- script/nvim.sh
+++ script/nvim.sh
@@ -7,13 +7,28 @@ fi
 
 shell=/bin/bash
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-$shell "${script_dir}/install_from_github_archive.sh" neovim neovim nvim-linux64.tar.gz
+archive_name=$(curl -s -H 'Accept: application/json' \
+	https://api.github.com/repos/neovim/neovim/releases/latest \
+	| jq -r '.assets[].name | select(test("nvim-linux.*x86_64\\.tar\\.gz"))')
+
+if [[ -z "${archive_name}" ]]; then
+	>&2 echo "Could not find a matching tar.gz asset for linux x86_64"
+	exit 1
+fi
+
+$shell "${script_dir}/install_from_github_archive.sh" neovim neovim "${archive_name}"
 if [[ $? -ne 0 ]]; then
 	exit $?
 fi
 
 path_tmp="/tmp/nvim_installation_$(date +%s)"
-path_from="${HOME}/.local/bin/nvim-linux64"
+# Detect extracted directory dynamically instead of hardcoding the name.
+path_from=$(find "${HOME}/.local/bin" -maxdepth 1 -type d -name "nvim-linux*" | head -1)
+
+if [[ -z "${path_from}" ]]; then
+	>&2 echo "Could not find extracted neovim directory in ${HOME}/.local/bin"
+	exit 1
+fi
 
 mv ${path_from} ${path_tmp}
 if [[ $? -ne 0 ]]; then
